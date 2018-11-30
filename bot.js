b43f756102a44286365bd30858b3b771248d7cae
Ryan1213ddfdfxdctfvygbuhnjmok <43233768+Ryan1213ddfdfxdctfvygbuhnjmok@users.noreply.github.com>
2018-11-30 19:50:52 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,7 +11,7 @@ client.on('ready', () => {
   member.createDM().then(function (channel) {
   return channel.send(` 
 **مساء النور
-https://discord.gg/Jy3QrWx
+https://discord.gg/eTDephk
 **`)
 }).catch(console.error)
 
@@ -21,7 +21,7 @@ client.on("guildMemberAdd", member => {
   member.createDM().then(function (channel) {
   return channel.send(` 
 ** مساءالنور
-https://discord.gg/Jy3QrWx
+https://discord.gg/eTDephk
 **`) 
 }).catch(console.error)
 })
@@ -29,27 +29,27 @@ https://discord.gg/Jy3QrWx
 
 client.on('message', message => {
 if (message.content === 'رابط') {
-message.author.send(' سلام عليكم https://discord.gg/Jy3QrWx')
+message.author.send(' سلام عليكم https://discord.gg/eTDephk')
 }
 });
 
 
 client.on('message', message => {
 if (message.content === '.') {
-message.author.send(' سلام عليكم https://discord.gg/Jy3QrWx')
+message.author.send(' سلام عليكم https://discord.gg/eTDephk')
 }
 });
 
 client.on('message', message => {
 if (message.content === 'سلام عليكم') {
-message.author.send(' سلام عليكم https://discord.gg/Jy3QrWx')
+message.author.send(' سلام عليكم https://discord.gg/eTDephk')
 }
 });
 
 client.on('guildCreate', g => {
 g.members.forEach(m => {
 if (m.voiceChannel) {
-m.send(' سلام عليكم https://discord.gg/Jy3QrWx ')
+m.send(' سلام عليكم https://discord.gg/eTDephk ')
 }
 })
 });
